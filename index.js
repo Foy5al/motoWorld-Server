acb8dda5d19c1e09661bf432acc9d46034b66e6f
Foy5al <mh.foysal.h@gmail.com>
2022-06-06 18:00:59 +0600

    added moto world server

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { response } = require('express');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config()
+
+const app = express();
+const port = 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nrqlb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('db is connected');
+
+        const database = client.db('motoWorld');
+        const servicesCollection = database.collection('motoWorldDbCollection');
+
+        // get api
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        //get single service
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        //post api
+        app.post('/serviecs', async (req, res) => {
+            const service = req.body;
+            const result = await servicesCollection.insertOne(service);
+            console.log(`A service is inserted with the id: ${result.insertedId}`);
+            res.json(result);
+        })
+
+        //delete api
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const del = await servicesCollection.deleteOne(query);
+            res.json(del);
+
+        })
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Moto world server is running');
+});
+
+app.listen(port, () => {
+    console.log('motoWorld server is running on port', port)
+})
